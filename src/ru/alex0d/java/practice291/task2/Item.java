885bfb12dfbc7e09aa75727bf405154993c4add0
alex0d <alexey.d707@gmail.com>
2022-12-08 02:34:50 +0300

    Removed unnecessary

--- src/ru/alex0d/java/practice291/task2/Item.java
+++
@@ -1,7 +0,0 @@
-package ru.alex0d.java.practice291.task2;
-
-public interface Item {
-    String getName();
-    String getDescription();
-    int getCost();
-}
--- src/ru/alex0d/java/practice291/task3/InternetOrder.java
+++
@@ -1,4 +0,0 @@
-package ru.alex0d.java.practice291.task3;
-
-public class InternetOrder {
-}
